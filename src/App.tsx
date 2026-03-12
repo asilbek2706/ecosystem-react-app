6d3a37f74c0a5aa82dccc18122f5b5c45da64c01
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-03-12 18:40:53 +0500

    feat: setup react router and basic app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import Login from './components/login.tsx';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import LoginPage from './auth/LoginPage';
+import NotFound from './components/NotFound.tsx';
+import AuthAction from './auth/AuthAction.tsx';
 
 function App() {
-    return <Login />;
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/forgot-password" element={<AuthAction />} />
+                <Route path="/activate" element={<AuthAction />} />
+
+                <Route path="/" element={<Navigate to="/login" replace />} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default App;
